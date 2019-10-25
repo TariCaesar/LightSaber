caa266cedc4cb887f32839e5685699e4ae32f2d7
TariCaesar <tarikhcy@gmail.com>
2019-10-25 13:19:14 +0800

    Add vscode launch file

--- Src/main.c
+++ Src/main.c
@@ -2,6 +2,16 @@
 
 int main(){
     DeviceInit();
+    //wait until usartinit complete
+    while(UsartInit());
+    char usartInitCompleteString[] = "Usart init complete";
+    UsartSendData((uint8_t*)usartInitCompleteString, sizeof(usartInitCompleteString));
 
+    while(1){
+        uint8_t temp;
+        if(!UsartReadBuffer(&temp)){
+            UsartSendData(&temp, 1);
+        }
+    }
     return 0;
 }
